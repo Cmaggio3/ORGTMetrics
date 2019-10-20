fd82760d605ed536a8bcbba82d9f34f8b6b5f94b
Cmaggio3 <Cmaggio3@gatech.edu>
2019-10-20 13:00:39 -0400

    Removed db field

--- database.js
+++ database.js
@@ -15,7 +15,7 @@ var db_config = {
 	host:"database-2.c5xkty4e7x1f.us-east-2.rds.amazonaws.com",
 	user: "admin",
 	password: "Testtobechanged12",
-	database: "database-2",
+	//database: "database-2",
     debug    :  true
 }
 
